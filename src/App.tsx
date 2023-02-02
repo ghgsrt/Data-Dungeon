478bb73d0124bf5b3c014ce4c528648782cfd246
ghgsrt <ajb7366@psu.edu>
2023-02-02 01:31:04 -0500

    Added convenient demo selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import { createSignal, Show } from 'solid-js';
+import { createSignal, For, JSX } from 'solid-js';
 import GameWindow from './components/QWOP/GameWindow';
 import Model from './components/Model.jsx';
-import SlideToggle from './components/SlideToggle';
+import InputExample from './components/InputExample';
+import Default from './components/Default';
 
 function App() {
-	const [playingQWOP, setPlayingQWOP] = createSignal(false);
+	const [currentDemoTitle, setCurrentDemoTitle] = createSignal<string>('');
+
+	const demoMenu: Record<string, JSX.Element> = {
+		Default: <Default />,
+		'useInput Example': <InputExample />,
+		Model: <Model />,
+		QWOP: <GameWindow />,
+	};
 
 	return (
-		<>
-			<div class="ml-52">
-				<span>Model </span>
-				<SlideToggle
-					onChange={(e: Event) =>
-						setPlayingQWOP((e.target as HTMLInputElement).checked)
-					}
-				/>
-				<span> QWOP</span>
+		<div class="flex w-screen h-screen bg-gray-800">
+			<div class="flex flex-col w-min h-screen">
+				<For each={Object.keys(demoMenu)}>
+					{(demoTitle) => (
+						<div
+							class={`flex-1 flex justify-center items-center p-1 
+									bg-gray-300 hover:bg-gray-400 
+									  select-none cursor-pointer 
+									  shadow-inner transition-colors
+									  active:bg-gray-400
+									  ${currentDemoTitle() === demoTitle ? 'bg-gray-400' : ''}`}
+							onClick={() => setCurrentDemoTitle(demoTitle)}
+						>
+							{demoTitle}
+						</div>
+					)}
+				</For>
 			</div>
-			<Show when={playingQWOP()} fallback={<Model />}>
-				<GameWindow />
-			</Show>
-		</>
+			<div class="flex-1 h-screen">{demoMenu[currentDemoTitle()]}</div>
+		</div>
 	);
 }
 
